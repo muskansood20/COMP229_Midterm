ae7db963d02359d7ad26850fd2f25b6811841438
Muskan Sood <sood.muskan2gmail.com>
2021-03-05 17:09:47 +0530

    Path changed for mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 /* Muskan Sood 301134477 Mid Term */
 module.exports = 
 {
-    "URI": "mongodb://localhost/book_store"
-    //"URI": "mongodb+srv://bhavya:wL2GDlGJuGHWp7EE@mongodbserver.l3jqy.mongodb.net/books_Store?retryWrites=true&w=majority"
+    //"URI": "mongodb://localhost/book_store"
+    "URI":"mongodb+srv://muskan:Hj1CLl4IQhnnyEKW@mongodbserver.x0a41.mongodb.net/book_store?retryWrites=true&w=majority"
 }
